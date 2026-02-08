d471401977c9390b72ec14c6966334752fd32eb9
x1unix <9203548+x1unix@users.noreply.github.com>
2026-02-08 15:31:43 -0500

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# M5Stack Atom Matrix Probe
+
+Playground project for ESP32-Pico-based [M5Stack Atom Matrix][devkit-url] devkit.
+
+[devkit-url]: https://shop.m5stack.com/products/atom-matrix-esp32-development-kit
+
+## Prerequisites
+
+Install following packages using `cargo install`:
+
+- `espflash`
+- `esp-config --features=tui`
+- `espup`
+  - Run `espup install` to install the xtensa target.
+  - Source `~/export-esp.sh` to your shell.
+
+### NeoVim
+
+Link the rust-analyzer path to make it work for `esp` target:
+
+```bash
+ln -sf ~/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/bin/rust-analyzer ~/.rustup/toolchains/esp/bin/rust-analyzer
+```
+
+## Resources/Docs
+
+- https://docs.m5stack.com/en/core/ATOM%20Matrix
+- https://docs.espressif.com/projects/rust/book/application-development/index.html
+- https://embassy.dev/
---
+++ src/bin/main.rs
@@ -0,0 +1,66 @@
+#![no_std]
+#![no_main]
+#![deny(
+    clippy::mem_forget,
+    reason = "mem::forget is generally not safe to do with esp_hal types, especially those \
+    holding buffers for the duration of a data transfer."
+)]
+#![deny(clippy::large_stack_frames)]
+
+use bt_hci::controller::ExternalController;
+use embassy_executor::Spawner;
+use embassy_time::{Duration, Timer};
+use esp_backtrace as _;
+use esp_hal::clock::CpuClock;
+use esp_hal::timer::timg::TimerGroup;
+use esp_radio::ble::controller::BleConnector;
+use log::info;
+use trouble_host::prelude::*;
+
+extern crate alloc;
+
+const CONNECTIONS_MAX: usize = 1;
+const L2CAP_CHANNELS_MAX: usize = 1;
+
+// This creates a default app-descriptor required by the esp-idf bootloader.
+// For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
+esp_bootloader_esp_idf::esp_app_desc!();
+
+#[allow(
+    clippy::large_stack_frames,
+    reason = "it's not unusual to allocate larger buffers etc. in main"
+)]
+#[esp_rtos::main]
+async fn main(spawner: Spawner) -> ! {
+    // generator version: 1.2.0
+
+    esp_println::logger::init_logger_from_env();
+
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let peripherals = esp_hal::init(config);
+
+    esp_alloc::heap_allocator!(#[esp_hal::ram(reclaimed)] size: 98768);
+
+    let timg0 = TimerGroup::new(peripherals.TIMG0);
+    esp_rtos::start(timg0.timer0);
+
+    info!("Embassy initialized!");
+
+    let radio_init = esp_radio::init().expect("Failed to initialize Wi-Fi/BLE controller");
+    // find more examples https://github.com/embassy-rs/trouble/tree/main/examples/esp32
+    let transport = BleConnector::new(&radio_init, peripherals.BT, Default::default()).unwrap();
+    let ble_controller = ExternalController::<_, 1>::new(transport);
+    let mut resources: HostResources<DefaultPacketPool, CONNECTIONS_MAX, L2CAP_CHANNELS_MAX> =
+        HostResources::new();
+    let _stack = trouble_host::new(ble_controller, &mut resources);
+
+    // TODO: Spawn some tasks
+    let _ = spawner;
+
+    loop {
+        info!("Hello world!");
+        Timer::after(Duration::from_secs(1)).await;
+    }
+
+    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0/examples
+}
